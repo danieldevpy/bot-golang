454668fbb4a9ca5602d9ed5121d9fdf19b04b442
Daniel <danielfernandes202@gmail.com>
2023-03-22 11:31:40 -0300

    fix

--- app/controller/messages.go
+++ app/controller/messages.go
@@ -2,21 +2,68 @@ package controller
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/danieldevpy/bot-golang/app/models"
 	"gorm.io/gorm"
 )
 
 func GetResponse(db *gorm.DB, botid int, number string, answer string) (*models.Profile, string) {
-
 	profile := GetUser(db, number, botid)
-	fmt.Println("inicio: ", profile)
 
 	if answer == "0" {
 		profile.Question = GetQuestionById(db, profile.Bot.MessageInicialID)
 	}
 
-	bot_answer := ProcessQuestion(db, profile, answer)
+	var questions_answers []models.Question
 
-	return profile, bot_answer
+	if !profile.Activate {
+		profile.Activate = true
+		questions_answers = append(questions_answers, *profile.Question)
+	}
+
+	if profile.Question.App {
+		ExecuteApp(db, profile, answer)
+		questions_answers = append(questions_answers, *profile.Question)
+		questions_answers = append(questions_answers, GetParents(db, profile.Question)...)
+	} else {
+		ProcessApp(db, profile)
+		if !profile.Question.Index {
+			questions_answers = append(questions_answers, *profile.Question)
+		}
+		parents := GetParents(db, profile.Question)
+		questions_answers = append(questions_answers, parents...)
+		lloop := 1
+		for loop := range questions_answers {
+			if questions_answers[loop].Index {
+				if answer == strconv.Itoa(lloop) {
+					profile.Question = &questions_answers[loop]
+					questions_answers = GetParents(db, profile.Question)
+					break
+				}
+				lloop = lloop + 1
+			}
+		}
+
+	}
+	if profile.Question.Final {
+		questions_answers = append(questions_answers, GetFinal(db, profile))
+		profile.Question = GetQuestionById(db, profile.Bot.MessageInicialID)
+	}
+
+	lrange := 1
+	object := ""
+	for loop := range questions_answers {
+		message := questions_answers[loop].Message
+		if message[:1] != "!" {
+			if questions_answers[loop].Index {
+				object = fmt.Sprintf("%s*%d* - %s", object, lrange, message+"|")
+				lrange = lrange + 1
+			} else {
+				object = object + message + "|"
+			}
+		}
+	}
+
+	return profile, object
 }
--- app/controller/question.go
+++ app/controller/question.go
@@ -2,7 +2,6 @@ package controller
 
 import (
 	"fmt"
-	"strconv"
 
 	"github.com/danieldevpy/bot-golang/app/models"
 	"gorm.io/gorm"
@@ -31,61 +30,4 @@ func GetFinal(db *gorm.DB, profile *models.Profile) models.Question {
 		fmt.Println("error")
 	}
 	return *question
-}
-func ProcessQuestion(db *gorm.DB, profile *models.Profile, answer string) string {
-	var questions_answers []models.Question
-
-	if !profile.Activate {
-		profile.Activate = true
-		questions_answers = append(questions_answers, *profile.Question)
-	}
-
-	if profile.Question.App {
-		ExecuteApp(db, profile, answer)
-		questions_answers = append(questions_answers, *profile.Question)
-		questions_answers = append(questions_answers, GetParents(db, profile.Question)...)
-	} else {
-		ProcessApp(db, profile)
-		fmt.Println("chamado fora de app")
-		if !profile.Question.Index {
-			questions_answers = append(questions_answers, *profile.Question)
-		}
-		parents := GetParents(db, profile.Question)
-		questions_answers = append(questions_answers, parents...)
-		fmt.Println("escolhas disponivel: ", questions_answers)
-		lloop := 1
-		for loop := range questions_answers {
-			if questions_answers[loop].Index {
-				if answer == strconv.Itoa(lloop) {
-					fmt.Println("caiu aqui escolha: ", questions_answers[loop])
-					profile.Question = &questions_answers[loop]
-					questions_answers = GetParents(db, profile.Question)
-					break
-				}
-				lloop = lloop + 1
-			}
-		}
-
-	}
-	fmt.Println("Q:", profile.Question)
-	if profile.Question.Final {
-		questions_answers = append(questions_answers, GetFinal(db, profile))
-		profile.Question = GetQuestionById(db, profile.Bot.MessageInicialID)
-	}
-
-	lrange := 1
-	object := ""
-	for loop := range questions_answers {
-		message := questions_answers[loop].Message
-		if message[:1] != "!" {
-			if questions_answers[loop].Index {
-				object = fmt.Sprintf("%s*%d* - %s", object, lrange, message+"|")
-				lrange = lrange + 1
-			} else {
-				object = object + message + "|"
-			}
-		}
-	}
-
-	return object
 }
